2c5cda8784c921c49621fefce9d3fb13053007fb
r2k1 <yokree@gmail.com>
2022-11-21 16:05:14 +1300

    create storage container if required

--- src/k8s-extension/azext_k8s_extension/partner_extensions/CostExport.py
+++ src/k8s-extension/azext_k8s_extension/partner_extensions/CostExport.py
@@ -1,40 +1,32 @@
 import logging
 
-from ..vendored_sdks.models import (
-    Extension,
-    Scope,
-    ScopeCluster,
-    PatchExtension
-)
-
 import datetime
-import json
 import time
 
 from azure.cli.core import AzCli
-from azure.core.exceptions import ResourceNotFoundError
 from azure.cli.core.azclierror import CLIInternalError, ValidationError
 from azure.cli.core.profiles import ResourceType
-from azure.mgmt.storage.v2022_05_01.models import StorageAccountCreateParameters, Sku, SkuName, Kind
 from .DefaultExtension import DefaultExtension
-from azure.cli.core.util import user_confirmation
 from azure.cli.core import get_default_cli
 from ..vendored_sdks.models import Extension
-from ..vendored_sdks.models import PatchExtension
 from ..vendored_sdks.models import ScopeCluster
-from ..vendored_sdks.models import ScopeNamespace
 from ..vendored_sdks.models import Scope
 from azure.cli.core.commands.client_factory import get_mgmt_service_client, get_subscription_id
-from azure.mgmt.storage import StorageManagementClient
 from knack.log import get_logger
 from logging import Logger
 
 logger: Logger = get_logger(__name__)
 logger.addHandler(logging.StreamHandler())
 
+
 class CostExport(DefaultExtension):
     def __init__(self):
         self.DEFAULT_RELEASE_NAMESPACE = 'cost-export'
+
+    # Configuration parameters:
+    # storage-account-resource-id
+    # storage-account-name
+    # storage-container-name
 
     def Create(self, cmd, client, resource_group_name, cluster_name, name, cluster_type, extension_type,
                scope, auto_upgrade_minor_version, release_train, version, target_namespace,
@@ -42,12 +34,28 @@ class CostExport(DefaultExtension):
                configuration_settings_file, configuration_protected_settings_file):
         logger.info("Creating CostExport extension")
 
-        _register_resource_provider(cmd, "Microsoft.CostManagementExports")
-        blob_storage_name = "arturcmdev"
-        storage_resource_group_name = "artur"
-        _create_cost_export(cmd, cluster_name=cluster_name, resource_group_name=resource_group_name,
-                            storage_account_name=blob_storage_name, storage_resource_group_name=storage_resource_group_name)
+        storage_account_name = configuration_settings.get('storage-account-name', None)
+        if not storage_account_name:
+            raise ValidationError("config parameter storage-account-name is required")
+        storage_container_name = configuration_settings.get('storage-container', "costexport")
+        storage_resource_group = configuration_settings.get('storage-resource-group', resource_group_name)
+        subscription: str = get_subscription_id(cmd.cli_ctx)
 
+        _ensure_storage_container(
+            subscription_id=subscription,
+            resource_group_name=storage_resource_group,
+            storage_account_name=storage_account_name,
+            storage_container_name=storage_container_name,
+        )
+        _register_resource_provider(cmd, "Microsoft.CostManagementExports")
+        # use cluster resource group if not specified
+        _create_cost_export(
+            subscription=subscription,
+            cluster_name=cluster_name,
+            resource_group_name=resource_group_name,
+            storage_account_name=storage_account_name,
+            storage_resource_group_name=storage_resource_group
+        )
 
         # Default validations & defaults for Create
         release_namespace = self.DEFAULT_RELEASE_NAMESPACE
@@ -118,9 +126,9 @@ def _providers_client_factory(cli_ctx, subscription_id=None):
                                    subscription_id=subscription_id).providers
 
 
-def _create_cost_export(cmd, cluster_name: str, storage_account_name: str, resource_group_name: str, storage_resource_group_name: str):
+def _create_cost_export(subscription: str, cluster_name: str, storage_account_name: str, resource_group_name: str,
+                        storage_resource_group_name: str):
     # TODO skip if exists
-    subscription: str = get_subscription_id(cmd.cli_ctx)
     mc_resource_group = _mc_resource_group(subscription=subscription, resource_group_name=resource_group_name,
                                            cluster_name=cluster_name)
     logger.info("creating cost export job for AKS cluster %s", mc_resource_group)
@@ -146,8 +154,6 @@ def _create_cost_export(cmd, cluster_name: str, storage_account_name: str, resou
     ]
     logger.info("running command: %s", " ".join(args))
     cli.invoke(args)
-    if cli.result.exit_code != 0:
-        raise Exception("unable to create costmanagement export")
     logger.info("cost export created")
 
 
@@ -161,5 +167,41 @@ def _mc_resource_group(subscription: str, resource_group_name: str, cluster_name
     return cli.result.result['nodeResourceGroup']
 
 
+def _ensure_storage_container(subscription_id: str, resource_group_name: str, storage_account_name: str,
+                                  storage_container_name: str):
+    # TODO: ensure create storage uses good security practices
+    logger.info("checking storage account %s, %s", storage_account_name, subscription_id)
+    create_storage_args = ["storage", "account", "create",
+                           "--resource-group", resource_group_name,
+                           "--name", storage_account_name,
+                           "--subscription", subscription_id,
+                           "--allow-blob-public-access", "false"]
+    _invoke(create_storage_args)  # TODO: error handling
+
+    logger.info(f"checking storage container {storage_container_name}, {storage_account_name}")
+    _invoke(["storage", "container", "create",
+             "--name", storage_container_name,
+             "--account-name", storage_account_name,
+             "--public-access", "off",
+             "--auth-mode", "login"
+             ])
+
+
 def _cli() -> AzCli:
     return get_default_cli()
+
+
+def _invoke(args) -> AzCli:
+    cli = _cli()
+    try:
+        cli.invoke(args)
+    except BaseException as e:
+        cmd = "az " + " ".join(args)
+        # TODO: is it helpful enough?
+        # it seems like the error message is logged inside invokation and isn't available here
+        logger.error(f"An error during setup step. Check your permissions or try to run it manually:\n{cmd}")
+        raise e
+    if cli.result.exit_code != 0:
+        cmd = "az " + " ".join(args)
+        raise Exception(f"Unexpected non-zero exit code ({cli.result.exit_code}) during command execution: az {cmd}")
+    return cli
