0b62bbbb989cc1bf01f99cf95306797f915200ee
longwan <longwan@microsoft.com>
2023-07-13 23:13:10 -0700

    update

--- src/k8s-extension/azext_k8s_extension/partner_extensions/ContainerInsights.py
+++ src/k8s-extension/azext_k8s_extension/partner_extensions/ContainerInsights.py
@@ -456,7 +456,7 @@ def _get_container_insights_settings(cmd, cluster_resource_group_name, cluster_r
     subscription_id = get_subscription_id(cmd.cli_ctx)
     workspace_resource_id = ''
     useAADAuth = True
-    configuration_settings['omsagent.useAADAuth'] = "true"
+    configuration_settings['amalogs.useAADAuth'] = "true"
     extensionSettings = {}
 
     if configuration_settings is not None:
