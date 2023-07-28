fc762892bda651ec0db5ce27378aa477f3bd0c40
bragi92 <kadubey@microsoft.com>
2023-07-28 14:12:44 -0700

    Update defaults.py

--- src/k8s-extension/azext_k8s_extension/partner_extensions/azuremonitormetrics/dc/defaults.py
+++ src/k8s-extension/azext_k8s_extension/partner_extensions/azuremonitormetrics/dc/defaults.py
@@ -14,8 +14,7 @@ from ..deaults import get_default_region
 # DCE remove underscore from cluster name
 def sanitize_name(name, objtype, length):
     name = name[0:length]
-    length = length - 
-    
+    length = length - 1
     if objtype == DC_TYPE.DCE:
         name = name.replace("_", "")
         name = ''.join(char for char in name if char.isalnum() or char == '-')
