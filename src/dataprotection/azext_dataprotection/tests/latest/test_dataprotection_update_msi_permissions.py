f500c3ffefecffaec4d2591bba0cd1bcce5bfd17
Bavneet Singh <33008256+bavneetsingh16@users.noreply.github.com>
2025-03-13 13:25:02 -0700

    Revert "{dataprotection} Updated packaged vendored SDK azure_mgmt_preview_aks to 2024-10-02-preview (#8544)"
    This reverts commit 841aa39883564565e275431ba85013a2ec43f78d.

--- src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_update_msi_permissions.py
+++ src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_update_msi_permissions.py
@@ -5,7 +5,6 @@
 # --------------------------------------------------------------------------
 # pylint: disable=line-too-long
 # pylint: disable=unused-import
-# WARNING: This test only works when run in the devbox.
 
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, live_only
 from azure.cli.testsdk.scenario_tests import AllowLargeResponse
